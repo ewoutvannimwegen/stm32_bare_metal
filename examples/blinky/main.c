4738cac81cc6da79de79a861b8da9f3abc1db804
ewoutvannimwegen <ewoutvannimwegen@gmail.com>
2021-01-30 09:46:41 +0100

    change stlinlv2.1 to stlink & fix clock init inside blinky

--- examples/blinky/main.c
+++ examples/blinky/main.c
@@ -1,6 +1,22 @@
 #include "stm32l476xx.h"
 
-int main() {
+int main(void) {
+	RCC->CR = 0; // Clear control register
+	RCC->CR |= RCC_CR_HSION; // HSI clock enabled
+	while(!(RCC->CR & RCC_CR_HSIRDY)); // Wait for HSI ready
+
+	RCC->APB1ENR1 |= RCC_APB1ENR1_PWREN; // Power interface clock enabled
+	PWR->CR1 &= ~PWR_CR1_VOS_1; 
+	PWR->CR1 |= PWR_CR1_VOS_0; // Voltage scaling range 1 
+
+	FLASH->ACR |= FLASH_ACR_ICEN; // Instruction cache enabled
+	FLASH->ACR |= FLASH_ACR_PRFTEN; // Prefetch enabled
+	FLASH->ACR |= FLASH_ACR_DCEN; // Data cache enabled
+	FLASH->ACR &= ~FLASH_ACR_LATENCY;
+	FLASH->ACR |= FLASH_ACR_LATENCY_2WS; // Flash latency 3 CPU cycles
+
+	RCC->CFGR |= RCC_CFGR_SW_HSI; // HSI as system clock
+	while(!(RCC->CFGR & RCC_CFGR_SWS_HSI)); 
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN; // GPIOA clock enabled
 
 	/* LD2 [PA5] */
@@ -10,7 +26,7 @@ int main() {
 	GPIOA->MODER |= GPIO_MODER_MODE5_0; // Output
 
 	while(1) {
+		for(int i = 0; i < SystemCoreClock; i++); // Sleep 1 sec
 		GPIOA->ODR ^= GPIO_ODR_OD5; // Toggle LD2
-		for(int i = 0; 100000; i++); // Sleep 1 sec
 	}
 }
